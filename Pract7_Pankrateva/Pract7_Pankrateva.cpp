9325127460cd6c05258a76476e3de960aaac51c9
KateePankratevaa <124819002+KateePankratevaa@users.noreply.github.com>
2024-02-10 12:32:42 +0500

    Add files via upload

---
+++ Pract7_Pankrateva/Pract7_Pankrateva.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int& maxdig(int num)
+{
+    setlocale(LC_ALL, "Russian");
+
+    static int max(0);
+
+    if (num)
+    {
+        if (num % 10 > max)
+            max = num % 10;
+    }
+    else
+        return max;
+
+    return maxdig(num / 10);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout <<"Наибольшее число десятичной записи неотрицательного числа: " << maxdig(1234856789) << endl;
+
+}
+
